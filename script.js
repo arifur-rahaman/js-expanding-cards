66d0ae87fe24be4275d307de511137bee1df82d1
arifur-rahaman <arif.rahman041@gmail.com>
2025-05-30 21:34:52 +0600

    feat: implement transition

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ const panels = document.querySelectorAll('.panel');
 
 // console.log(panels[0])
 panels.forEach((panel) =>{
-    console.log(panel)
+    panel.addEventListener('click', () =>{
+        removeActiveClasses();
+        panel.classList.add('active')
+    })
 })
+
+
+function removeActiveClasses(){
+    panels.forEach(panel =>{
+        panel.classList.remove('active')
+    })
+}
